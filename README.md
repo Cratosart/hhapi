d574063e11f0046ed6f20a73608de82471279adb
Artyom <artem.a.dyachenko@gmail.com>
2022-01-09 17:17:44 +0300

    First publication of the application

--- README.md
+++ README.md
@@ -1,3 +1,80 @@
+# Programming vacancies compare
+
+
+The program searches for current vacancies on the portals and displays them in the console in the form of a table.
+
+
+### How to install
+
+Python3 must already be installed.
+Then use 'pip' to install the dependencies.
+
 ```
-$ python -m pip install requests
+
+pip install -r requirements.txt
+
 ```
+```
+py main.py
+```
+
+### Environment variables
+
+The program takes keys from the variable environment.
+Place an .env file in the root directory with the following parameters:
+
+
+```
+API_KEY_SJ=there must be a SuperJob API key
+
+```
+
+API_KEY_SJ=used to connect to API SuperJob for example:
+```
+v3.r.15029133.2ff49f9a025d973798236d8e53264e253854ab29.5e9687b65b85e460306e83c5b8c97a858f8b4eb3
+```
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
+
+# Сравниваем вакансии программистов
+
+Срикпт осуществляет поиск акутальных вакансий на порталах и вывод их в консоль в виде таблицы.
+
+### Как установить
+
+Python3 должен быть уже установлен.
+Затем воспользуйтесь 'pip' для установки зависимостей.
+
+```
+
+pip install -r requirements.txt
+
+```
+
+```
+py main.py
+```
+
+### Переменные окружения
+
+Программа берёт ключи из переменного окружения.
+Положите в корневую директорию файл .env в котором должны быть следующие параметры:
+
+
+```
+API_KEY_SJ=тут доолжен быть API ключ SuperJob
+
+```
+
+API_KEY_SJ= используется для подключения к API SuperJob например:
+```
+v3.r.15029133.2ff49f9a025d973798236d8e53264e253854ab29.5e9687b65b85e460306e83c5b8c97a858f8b4eb3
+```
+
+
+Рекомендуется использовать [virtualenv/venv](https://docs.python.org/3/library/venv.html) для изоляции проекта.
+
+### Цель проекта
+
+Код написан в образовательных целях на онлайн-курсе для веб-разработчиков [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -1,38 +1,112 @@
+from itertools import count
+import os
 import requests
-prog={}
+from terminaltables import AsciiTable
+
+
+def predict_rub_salary(vacancy):
+    salary = vacancy['salary']
+    try:
+        if salary['from'] is not None and salary['currency'] == 'RUR':
+            if salary['to'] is not None:
+                zarplata = (salary['from'] + salary['to']) / 2  # Зарплата от нормальных значений
+                return zarplata
+            elif salary['to'] is None:
+                zarplata = salary['from'] * 1.2  # Зарплата от значения "от"
+                return zarplata
+            else:
+                return None
+        elif salary['from'] is None and salary['to'] is not None:
+            zarplata = salary['to'] * 0.8  # Зарплата до значения "до"
+            return zarplata
+        else:
+            return None
+    except:
+        return None
+
+
+def predict_rub_salary_for_superJob(vacancy):
+    salary = (int((vacancy['payment_from'] + vacancy['payment_to']) / 2))
+    if salary is not None:
+        return salary
+
+
+def create_table(dict_date):
+    table_data = [['Язык программирования', 'Вакансий найдено', 'Вакансий использовано', 'Средняя зарплата']]
+    for key, value in dict_date.items():
+        lang = key
+        found = dict_date[key]['vacancies_found']
+        proc = dict_date[key]['vacancies_processed']
+        ave = int(dict_date[key]['average_salary'])
+        table_data.append([lang, found, proc, ave])
+    return table_data
+
+
+prog = {}
+salary = []
 top_programming_language = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C"]
 
 for language in top_programming_language:
-    url = 'https://api.hh.ru/vacancies'
-    payload = {'text': language,
-               'area': '1',
-                'period': '30'}
-    info = requests.get(url, params=payload)
-    counter = info.json()['found']
-    prog[language] = counter
-    print(prog)
-    test2 = info.json()['items']
-    for work in test2:
-        print(work['name'])
-        print(work['salary'])
+    for page in count(0):
+        url = 'https://api.hh.ru/vacancies'
+        payload = {'text': language,
+                   'area': '1',
+                   'period': '30',
+                   'page': page
+                   }
+        info = requests.get(url, params=payload)
+        info.raise_for_status()
+        counter = info.json()['found']
+        # prog[language] = counter
+        vacancies = info.json()['items']
+        if page >= info.json()['pages'] - 1:
+            salary.clear()
+            break
+        if language == language:
+            for job_vacancy in vacancies:
+                zarplata = predict_rub_salary(job_vacancy)
+                if zarplata is not None:
+                    salary.append(zarplata)
+                    itog = (int(sum(salary) / (len(salary))))
+                    prog[language] = {"vacancies_found": counter, "vacancies_processed": len(salary),
+                                      "average_salary": itog}
 
 
-    # "items": [
-    #     {
-    #         "id": "48457284",
-    #         "premium": false,
-    #         "name": "Разработчик Solidity/Разработчик Rust/Разработчик Go/Разработчтик python(удаленно)",
-    #         "department": null,
-    #         "has_test": false,
-    #         "response_letter_required": false,
-    #         "area": {
-    #             "id": "1",
-    #             "name": "Москва",
-    #             "url": "https://api.hh.ru/areas/1"
-    #         },
-    #         "salary": {
-    #             "from": 600000,
-    #             "to": null,
-    #             "currency": "RUR",
-    #             "gross": false
+API_KEY_SJ = os.environ['API_KEY_SJ']
+prog_sj = {}
+money = []
+url = 'https://api.superjob.ru/2.0/vacancies/'
+headers = {
+    'X-Api-App-Id': API_KEY_SJ
+    }
+for language in top_programming_language:
+    for page in count(0):
+        payload = {
+            'keyword': f'Программист {language}',
+            'geo[t][0]': 4,
+            'page': page
+        }
+        job_sj = requests.get(url, headers=headers, params=payload)
+        job_sj.raise_for_status()
+        counter = job_sj.json()['total']
+        info = job_sj.json()['objects']
+        if job_sj.json()['more'] == False:
+            money.clear()
+            break
+        for job in info:
+            salary = (predict_rub_salary_for_superJob(job))
+            money.append(salary)
+            itog = sum(money) / len(money)
+            prog_sj[language] = {"vacancies_found": counter, "vacancies_processed": len(money), "average_salary": itog}
 
+table_data_sj = create_table(prog_sj)
+title = "Work on SuperJob Moscow"
+table = AsciiTable(table_data_sj, title)
+table.inner_row_border = True
+print(table.table)
+
+table_data_hh = create_table(prog)
+title = "Work on HeadHunter Moscow"
+table = AsciiTable(table_data_hh, title)
+table.inner_row_border = True
+print(table.table)
